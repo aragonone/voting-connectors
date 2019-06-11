aba8283cf0a53bf68198da06b42141f53c933f1e
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-06-11 15:24:59 -0300

    Token wrapper example (#8)
    * melon: ignore package-lock
    
    * token-wrapper: add token-wrapper example
    
    * token-wrapper: assert unlock amount is greater than zero
    
    * token-wrapper: drop change token controller function

---
+++ contracts/TokenWrapper.sol
@@ -0,0 +1,135 @@
+pragma solidity 0.4.24;
+
+import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/contracts/common/IForwarder.sol";
+import "@aragon/os/contracts/lib/token/ERC20.sol";
+import "@aragon/os/contracts/lib/math/SafeMath.sol";
+import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/apps-shared-minime/contracts/ITokenController.sol";
+
+
+/**
+ * @title TokenWrapper
+ * @dev Based on https://github.com/MyBitFoundation/MyBit-DAO.tech/blob/master/apps/MyTokens/contracts/MyTokens.sol
+ */
+contract TokenWrapper is ITokenController, IForwarder, AragonApp {
+    using SafeMath for uint256;
+
+    string private constant ERROR_CAN_NOT_FORWARD = "TW_CAN_NOT_FORWARD";
+    string private constant ERROR_CALLER_NOT_TOKEN = "TW_CALLER_NOT_TOKEN";
+    string private constant ERROR_LOCK_AMOUNT_ZERO = "TW_LOCK_AMOUNT_ZERO";
+    string private constant ERROR_UNLOCK_AMOUNT_ZERO = "TW_UNLOCK_AMOUNT_ZERO";
+    string private constant ERROR_INVALID_UNLOCK_AMOUNT = "TW_INVALID_UNLOCK_AMOUNT";
+    string private constant ERROR_INVALID_TOKEN_CONTROLLER = "TW_INVALID_TOKEN_CONTROLLER";
+    string private constant ERROR_TOKEN_BURN_FAILED = "TW_TOKEN_BURN_FAILED";
+    string private constant ERROR_TOKEN_MINT_FAILED = "TW_TOKEN_MINT_FAILED";
+    string private constant ERROR_ERC20_TRANSFER_FAILED = "TW_ERC20_TRANSFER_FAILED";
+    string private constant ERROR_ERC20_TRANSFER_FROM_FAILED = "TW_ERC20_TRANSFER_FROM_FAILED";
+
+    ERC20 public erc20;
+    MiniMeToken public token;
+    mapping(address => uint256) internal lockedAmount;
+
+    event TokensLocked(address entity, uint256 amount);
+    event TokensUnlocked(address entity, uint256 amount);
+
+    modifier onlyToken() {
+        require(msg.sender == address(token), ERROR_CALLER_NOT_TOKEN);
+        _;
+    }
+
+    /**
+     * @notice Initialize a new MiniMe token that will be the controller of an ERC20 token
+     * @param _token The MiniMe token that is used in the DAO
+     * @param _erc20 The ERC20 token that is locked in order to receive MiniMe tokens
+     */
+    function initialize(MiniMeToken _token, ERC20 _erc20) external {
+        initialized();
+        require(_token.controller() == address(this), ERROR_INVALID_TOKEN_CONTROLLER);
+
+        token = _token;
+        erc20 = _erc20;
+        token.enableTransfers(false);
+    }
+
+    /**
+     * @notice Lock `_amount` tokens
+     */
+    function lock(uint256 _amount) external {
+        require(_amount > 0, ERROR_LOCK_AMOUNT_ZERO);
+
+        lockedAmount[msg.sender] = lockedAmount[msg.sender].add(_amount);
+        emit TokensLocked(msg.sender, _amount);
+
+        require(erc20.transferFrom(msg.sender, address(this), _amount), ERROR_ERC20_TRANSFER_FROM_FAILED);
+        require(token.generateTokens(msg.sender, _amount), ERROR_TOKEN_MINT_FAILED);
+    }
+
+    /**
+     * @notice Unlock `_amount` tokens
+     */
+    function unlock(uint256 _amount) external {
+        require(_amount > 0, ERROR_UNLOCK_AMOUNT_ZERO);
+        require(_amount <= lockedAmount[msg.sender], ERROR_INVALID_UNLOCK_AMOUNT);
+
+        lockedAmount[msg.sender] = lockedAmount[msg.sender].sub(_amount);
+        emit TokensUnlocked(msg.sender, _amount);
+
+        require(token.destroyTokens(msg.sender, _amount), ERROR_TOKEN_BURN_FAILED);
+        require(erc20.transfer(msg.sender, _amount), ERROR_ERC20_TRANSFER_FAILED);
+    }
+
+    /*
+    * @dev Notifies the controller about a token transfer allowing the controller to react if desired
+    * @return False aways
+    */
+    function onTransfer(address, address, uint256) external onlyToken returns (bool) {
+        return false;
+    }
+
+    /**
+     * @dev Notifies the controller about an approval allowing the controller to react if desired
+     * @return False always
+     */
+    function onApprove(address, address, uint256) external onlyToken returns (bool) {
+        return false;
+    }
+
+    /**
+     * @dev Notifies the controller when the token contract receives ETH allowing the controller to react if desired
+     * @return False always
+     */
+    function proxyPayment(address) external payable onlyToken returns (bool) {
+        return false;
+    }
+
+    /**
+     * @notice Execute desired action as a token holder
+     * @dev IForwarder interface conformance. Forwards any token holder action.
+     * @param _evmScript Script being executed
+     */
+    function forward(bytes _evmScript) public {
+        require(canForward(msg.sender, _evmScript), ERROR_CAN_NOT_FORWARD);
+        bytes memory input = new bytes(0);
+
+        // Add the managed token to the blacklist to disallow a token holder from executing actions
+        // on the token controller's (this contract) behalf
+        address[] memory blacklist = new address[](2);
+        blacklist[0] = address(token);
+        blacklist[1] = address(erc20);
+
+        runScript(_evmScript, input, blacklist);
+    }
+
+    function isForwarder() public pure returns (bool) {
+        return true;
+    }
+
+    function canForward(address _sender, bytes) public view returns (bool) {
+        return hasInitialized() && token.balanceOf(_sender) > 0;
+    }
+
+    function getLockedAmount(address _account) public view returns (uint256) {
+        return lockedAmount[_account];
+    }
+}
---
+++ test/token_wrapper.js
@@ -0,0 +1,72 @@
+const { assertRevert } = require('@aragon/test-helpers/assertThrow')
+
+const ACL = artifacts.require('ACL')
+const Kernel = artifacts.require('Kernel')
+const ERC20 = artifacts.require('ERC20Sample')
+const MiniMeToken = artifacts.require('MiniMeToken')
+const TokenWrapper = artifacts.require('TokenWrapper')
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+contract('TokenWrapper', ([_, root, holder, someone]) => {
+  let dao, acl, tokenWrapper, erc20, token
+
+  before('deploy dao with token wrapper', async () => {
+    dao = await Kernel.new(false)
+    const aclBase = await ACL.new()
+    await dao.initialize(aclBase.address, root)
+    acl = ACL.at(await dao.acl())
+    await acl.createPermission(root, dao.address, await dao.APP_MANAGER_ROLE(), root, { from: root })
+
+    const tokenWrapperBase = await TokenWrapper.new()
+    const { logs } = await dao.newAppInstance('0x1234', tokenWrapperBase.address, '0x', false, { from: root })
+    tokenWrapper = TokenWrapper.at(logs.find(l => l.event === 'NewAppProxy').args.proxy)
+
+    erc20 = await ERC20.new({ from: holder }) // mints 1M e 18 tokens to sender
+    token = await MiniMeToken.new(ZERO_ADDRESS, ZERO_ADDRESS, 0, 'Token', 18, 'TWR', false, { from: root })
+    await token.changeController(tokenWrapper.address, { from: root })
+    await tokenWrapper.initialize(token.address, erc20.address)
+  })
+
+  it('has an erc20 and a minime token', async () => {
+    assert.isTrue(await tokenWrapper.isForwarder())
+    assert.equal(await tokenWrapper.erc20(), erc20.address)
+    assert.equal(await tokenWrapper.token(), token.address)
+  })
+
+  it('can mint tokens', async () => {
+    await erc20.approve(tokenWrapper.address, 2e18, { from: holder })
+    await tokenWrapper.lock(2e18, { from: holder })
+
+    assert.isTrue(await tokenWrapper.canForward(holder, '0x'))
+    assert.equal((await tokenWrapper.getLockedAmount(holder)).toString(), 2e18)
+    assert.equal((await token.balanceOf(holder)).toString(), 2e18)
+    assert.equal((await erc20.balanceOf(holder)).toString(), 999998e18)
+  })
+
+  it('can not mint invalid amounts', async () => {
+    await assertRevert(tokenWrapper.lock(0, { from: holder }), 'TW_LOCK_AMOUNT_ZERO')
+    await assertRevert(tokenWrapper.lock(1e30, { from: holder }), 'TW_ERC20_TRANSFER_FROM_FAILED')
+  })
+
+  it('can burn tokens', async () => {
+    await tokenWrapper.unlock(1e18, { from: holder })
+
+    assert.equal((await tokenWrapper.getLockedAmount(holder)).toString(), 1e18)
+    assert.equal((await token.balanceOf(holder)).toString(), 1e18)
+    assert.equal((await erc20.balanceOf(holder)).toString(), 999999e18)
+  })
+
+  it('can not burn invalid amounts', async () => {
+    await assertRevert(tokenWrapper.unlock(0, { from: holder }), 'TW_UNLOCK_AMOUNT_ZERO')
+    await assertRevert(tokenWrapper.unlock(1e30, { from: holder }), 'TW_INVALID_UNLOCK_AMOUNT')
+  })
+
+  it('does not allow to transfer tokens', async () => {
+    await assertRevert(token.transfer(someone, 1e16, { from: holder }))
+  })
+
+  it('does not allow to approve tokens', async () => {
+    await assertRevert(token.approve(someone, 1e16, { from: holder }))
+  })
+})
