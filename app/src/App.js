77da32907ea2f3858c3b6a6225f798864cb14722
delfipolito <delfinapolito@gmail.com>
2019-10-17 14:26:43 -0300

    Empty state screen

--- app/src/App.js
+++ app/src/App.js
@@ -1,51 +1,37 @@
-import React from 'react'
-import { useAragonApi } from '@aragon/api-react'
-import { Main, Button } from '@aragon/ui'
-import styled from 'styled-components'
-import BN from 'bn.js'
+import React from "react";
+import { useAragonApi } from "@aragon/api-react";
+import { Main, Button } from "@aragon/ui";
+import styled from "styled-components";
+import BN from "bn.js";
+import NoWrappedTokens from "./screens/NoWrappedTokens";
 
 function App() {
-  const { api, appState } = useAragonApi()
-  const {
-    token,
-    erc20,
-    tokenBalance,
-    erc20Balance,
-    isSyncing
-  } = appState
+  const { api, appState } = useAragonApi();
+  const { token, erc20, tokenBalance, erc20Balance, isSyncing } = appState;
 
   // TODO: Read this from an input component
-  const amount = '10000000'
+  const amount = "10000000";
 
   return (
     <Main>
       <BaseLayout>
         {isSyncing && <Syncing />}
-        { tokenBalance && tokenBalance > 0 ?
-            <div>
-              <p>You have {tokenBalance} org tokens </p>
-              <Button mode="secondary" onClick={async () => await api.unlock(amount).toPromise()}>
-                Unlock tokens
-              </Button>
-            </div>
-            :
-            <div>
-              <p>You dont have org tokens but you have {erc20Balance} of erc20 tokens to wrap</p>
-              <Button mode="secondary" onClick={
-                async () => {
-                  const app = (await api.currentApp().toPromise()).appAddress
-                  const intentParams = { token: { address: erc20, value: amount, spender: app } }
-                  await api.lock(amount, intentParams).toPromise()
-                }
-              }>
-                Lock tokens
-              </Button>
-            </div>
-        }
-
+        {tokenBalance && tokenBalance > 0 ? (
+          <div>
+            <p>You have {tokenBalance} org tokens </p>
+            <Button
+              mode="secondary"
+              onClick={async () => await api.unlock(amount).toPromise()}
+            >
+              Unlock tokens
+            </Button>
+          </div>
+        ) : (
+          <NoWrappedTokens isSyncing={isSyncing} />
+        )}
       </BaseLayout>
     </Main>
-  )
+  );
 }
 
 const BaseLayout = styled.div`
@@ -54,23 +40,23 @@ const BaseLayout = styled.div`
   justify-content: center;
   height: 100vh;
   flex-direction: column;
-`
+`;
 
 const Count = styled.h1`
   font-size: 30px;
-`
+`;
 
 const Buttons = styled.div`
   display: grid;
   grid-auto-flow: column;
   grid-gap: 40px;
   margin-top: 20px;
-`
+`;
 
-const Syncing = styled.div.attrs({ children: 'Syncing…' })`
+const Syncing = styled.div.attrs({ children: "Syncing…" })`
   position: absolute;
   top: 15px;
   right: 20px;
-`
+`;
 
-export default App
+export default App;
---
+++ app/src/screens/NoWrappedTokens.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Button, EmptyStateCard, GU, LoadingRing } from "@aragon/ui";
+import emptyStateImg from "../assets/empty-state.svg";
+import { useAragonApi } from '@aragon/api-react'
+
+const NoWrappedTokens = React.memo(function NoWrappedTokens({ isSyncing }) {
+  const { api, appState } = useAragonApi()
+  const {
+    token,
+    erc20,
+    tokenBalance,
+    erc20Balance,
+  } = appState
+
+  // TODO: Read this from an input component
+  const amount = '10000000'
+  
+  return (
+    <EmptyStateCard
+      text={
+        isSyncing ? (
+          <div
+            css={`
+              display: grid;
+              align-items: center;
+              justify-content: center;
+              grid-template-columns: auto auto;
+              grid-gap: ${1 * GU}px;
+            `}
+          >
+            <LoadingRing />
+            <span>Syncing…</span>
+          </div>
+        ) : (
+          "No tokens here!"
+        )
+      }
+      action={
+        <Button
+          wide
+          mode="strong"
+          onClick={async () => {
+            const app = (await api.currentApp().toPromise()).appAddress;
+            const intentParams = {
+              token: { address: erc20, value: amount, spender: app }
+            };
+            await api.lock(amount, intentParams).toPromise();
+          }}
+        >
+          Wrap token
+        </Button>
+      }
+      illustration={
+        <img
+          css={`
+            margin: auto;
+            height: 170px;
+          `}
+          src={emptyStateImg}
+          alt="No tokens here"
+        />
+      }
+    />
+  );
+});
+
+export default NoWrappedTokens;
