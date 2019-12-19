86b4c6ef2865a1f23f82a67ccaed0a1247127489
ßingen <bingen@aragon.one>
2019-12-19 11:42:04 +0100

    Add Active Period tests

---
+++ shared/contract-utils/contracts/test/TestImports.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.24;
+
+import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
+
+
+// HACK to workaround truffle artifact loading on dependencies
+contract Imports {
+    // solium-disable-previous-line no-empty-blocks
+}
