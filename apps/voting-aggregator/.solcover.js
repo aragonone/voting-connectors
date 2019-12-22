9a91a8c6b4249508c2cfbd7be41004b2a49d6347
Brett Sun <qisheng.brett.sun@gmail.com>
2019-12-22 23:20:51 +0700

    Add .solcover configuration

---
+++ apps/voting-aggregator/.solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+    norpc: true,
+    copyPackages: ['@aragon/os', '@aragonone/voting-connectors-contract-helpers'],
+    skipFiles: [
+        'test',
+        '@aragon/os',
+        '@aragonone/voting-connectors-contract-helpers',
+    ]
+}
