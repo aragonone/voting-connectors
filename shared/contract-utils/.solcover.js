1c2db9491c8afd023753350d7398901afc662a2f
Brett Sun <qisheng.brett.sun@gmail.com>
2019-12-23 03:40:56 +0700

    contract-utils: add interfaces to coverage skip list

--- shared/contract-utils/.solcover.js
+++ shared/contract-utils/.solcover.js
@@ -1,8 +1,9 @@
 module.exports = {
-    norpc: true,
-    copyPackages: ['@aragon/os'],
-    skipFiles: [
-        'test',
-        '@aragon/os',
-    ]
+  norpc: true,
+  copyPackages: ['@aragon/os'],
+  skipFiles: [
+    'interfaces',
+    'test',
+    '@aragon/os',
+  ]
 }
