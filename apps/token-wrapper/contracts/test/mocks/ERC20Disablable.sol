8ce5e6ee4e2ec409323db65b954ee5e830b810e4
Brett Sun <qisheng.brett.sun@gmail.com>
2019-12-23 11:48:48 +0700

    TokenWrapper: rename ERC20Enablable mock to ERC20Disabable

--- apps/token-wrapper/contracts/test/mocks/ERC20Disablable.sol
+++ apps/token-wrapper/contracts/test/mocks/ERC20Disablable.sol
@@ -11,18 +11,20 @@ contract ERC20Disablable is ERC20 {
     uint256 public _totalSupply = 1e6 * 10**18;
     mapping(address => uint256) private balances;
     mapping(address => mapping (address => uint256)) private allowed;
-    bool enabled;
+    bool disabled;
 
     constructor () public {
         balances[msg.sender] = _totalSupply;
     }
 
-    function enable(bool _enabled) external {
-        enabled = _enabled;
+    function disable(bool _disabled) external {
+        disabled = _disabled;
     }
 
     function transfer(address _to, uint256 _amount) public returns (bool success) {
-        if (enabled) {
+        if (disabled) {
+            return false;
+        } else {
             /* solium-disable-next-line */
             if (balances[msg.sender] >= _amount && _amount > 0 && balances[_to] + _amount > balances[_to]) {
                 balances[msg.sender] -= _amount;
@@ -31,13 +33,13 @@ contract ERC20Disablable is ERC20 {
             } else {
                 return false;
             }
-        } else {
-            return false;
         }
     }
 
     function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
-        if (enabled) {
+        if (disabled) {
+            return false;
+        } else {
             /* solium-disable-next-line */
             if (balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount > 0 && balances[_to] + _amount > balances[_to]) {
                 balances[_from] -= _amount;
@@ -47,17 +49,15 @@ contract ERC20Disablable is ERC20 {
             } else {
                 return false;
             }
-        } else {
-            return false;
         }
     }
 
     function approve(address _spender, uint256 _amount) public returns (bool success) {
-        if (enabled) {
+        if (disabled) {
+            return false;
+        } else {
             allowed[msg.sender][_spender] = _amount;
             return true;
-        } else {
-            return false;
         }
     }
 
