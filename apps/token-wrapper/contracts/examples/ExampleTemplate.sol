8ae04857a6b5d4e6fc7876b6a43ddd0579b0de3c
Brett Sun <qisheng.brett.sun@gmail.com>
2020-01-07 03:31:49 +0800

    chore: upgrade aragonCLI to v7

--- apps/token-wrapper/contracts/examples/ExampleTemplate.sol
+++ apps/token-wrapper/contracts/examples/ExampleTemplate.sol
@@ -16,7 +16,8 @@ contract TemplateBase is APMNamehash {
     ENS public ens;
     DAOFactory public fac;
 
-    event DeployInstance(address dao);
+    event DeployDao(address dao);
+    event SetupDao(address dao);
     event InstalledApp(address appProxy, bytes32 appId);
 
     constructor(DAOFactory _fac, ENS _ens) public {
@@ -49,6 +50,8 @@ contract ExampleTemplate is TemplateBase {
 
     function newInstance(ERC20 _depositedToken) public {
         Kernel dao = fac.newDAO(this);
+        emit SetupDao(dao);
+
         ACL acl = ACL(dao.acl());
         acl.createPermission(this, dao, dao.APP_MANAGER_ROLE(), this);
 
@@ -78,6 +81,6 @@ contract ExampleTemplate is TemplateBase {
         acl.revokePermission(this, acl, acl.CREATE_PERMISSIONS_ROLE());
         acl.setPermissionManager(root, acl, acl.CREATE_PERMISSIONS_ROLE());
 
-        emit DeployInstance(dao);
+        emit SetupDao(dao);
     }
 }
